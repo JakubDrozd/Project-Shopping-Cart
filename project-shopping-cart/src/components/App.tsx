b30e3a48d1489078a88efee83acaa4862f8df607
Veanty <plmulticraftpl@gmail.com>
2022-08-29 18:42:19 +0200

    Feat: add cart route

--- project-shopping-cart/src/components/App.tsx
+++ project-shopping-cart/src/components/App.tsx
@@ -3,6 +3,7 @@ import { Home } from "./Home"
 import { Shop } from "./Shop"
 import { NotFound } from "./NotFound"
 import {Nav} from "./Nav"
+import { Cart } from "./Cart"
 
 
 export function App(){
@@ -13,6 +14,7 @@ export function App(){
             <Route path="/" element={<Home></Home>}></Route>
             <Route path="/shop" element={<Shop></Shop>}></Route>
             <Route path="*" element={<NotFound></NotFound>}></Route>
+            <Route path="/cart" element={<Cart></Cart>}></Route>
           </Routes>
         </BrowserRouter>
     )

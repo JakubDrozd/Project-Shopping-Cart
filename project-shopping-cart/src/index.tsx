bf9e0b0c8acf29b4a13964fed11e381015a24bb1
Veanty <plmulticraftpl@gmail.com>
2022-08-29 17:34:47 +0200

    Refactor: change root render to app

--- project-shopping-cart/src/index.tsx
+++ project-shopping-cart/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
+import "./styles/index.css"
+import { Home } from './Home';
+import { App } from './App';
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App></App>
   </React.StrictMode>
 );
 

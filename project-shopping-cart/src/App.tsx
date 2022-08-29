6755ddf829639645d7854450492181f016987b81
Veanty <plmulticraftpl@gmail.com>
2022-08-29 17:45:52 +0200

    Feat: now navbar isnt reloaded with changing routes

--- project-shopping-cart/src/App.tsx
+++ project-shopping-cart/src/App.tsx
@@ -1,15 +1,19 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { Home } from "./Home"
 import { Shop } from "./Shop"
+import { NotFound } from "./NotFound"
+import {Nav} from "./Nav"
 
 
 export function App(){
     return (
         <BrowserRouter>
-            <Routes>
-                    <Route index element={<Home></Home>}></Route>
-                    <Route path="/shop" element={<Shop></Shop>}></Route>
-            </Routes>
+        <Nav />
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/shop" element={<Shop></Shop>}></Route>
+            <Route path="*" element={<NotFound></NotFound>}></Route>
+          </Routes>
         </BrowserRouter>
     )
 }

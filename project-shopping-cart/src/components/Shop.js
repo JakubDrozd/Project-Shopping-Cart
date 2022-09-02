96d640ddf78a699d13e88cb5ce3aaa72283372a5
Veanty <plmulticraftpl@gmail.com>
2022-09-02 17:42:31 +0200

    Feat: fix styling and add new banner components

--- project-shopping-cart/src/components/Shop.js
+++ project-shopping-cart/src/components/Shop.js
@@ -21,9 +21,9 @@ export function Shop() {
     <>
       <div>
         {/* Header*/}
-        <header className="bg-dark py-5">
-          <div className="container px-4 px-lg-5 my-5">
-            <div className="text-center text-white">
+        <header className="py-5 announce">
+          <div className="container px-4 px-lg-5 my-5 ">
+            <div className="text-center text-white ">
               <h1 className="display-4 fw-bolder">Today's store</h1>
               <p className="lead fw-normal text-white-50 mb-0">
                 Updates at 1 AM CET
@@ -36,10 +36,7 @@ export function Shop() {
           <div className="container px-4 px-lg-5 mt-5">
             <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
               {items.map((item) => {
-                if (
-                  item.item.images.background === undefined ||
-                  item.store.cost === 0
-                ) {
+                if (item.store.cost === 0) {
                   return;
                 }
                 return (

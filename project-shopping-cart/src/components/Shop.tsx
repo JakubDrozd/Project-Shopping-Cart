eea17bca28da156affa82d106f204e8e858a11e0
Veanty <plmulticraftpl@gmail.com>
2022-09-02 16:47:24 +0200

    Feat: add fetching items from the store

--- project-shopping-cart/src/components/Shop.tsx
+++ project-shopping-cart/src/components/Shop.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react"
 
 
 export function Shop(){
+
+    const [items, setItems] = useState([])
+
+    useEffect(()=>{
+        fetchItems()
+        console.log(items)
+    },[])
+
+    const fetchItems = async ()=>{
+        const data = fetch("https://fortnite-api.theapinetwork.com/store/get")
+        const items = (await data).json()
+        setItems(await items)
+    }
+
     return (
         <>
             <h1>Shop</h1>

cb85203ca83db9e3038d062eadda69573f033544
Veanty <plmulticraftpl@gmail.com>
2022-09-03 14:48:48 +0200

    Refactor: delete useless states

---
+++ project-shopping-cart/src/components/Nav.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { CartDropdown } from "./Dropdown";
+// import {Button} from "bootstrap//BUtton"
+
+export function Nav() {
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <li>Home</li>
+      </Link>
+      <Link to="/shop">
+        <li>Shop</li>
+      </Link>
+      <CartDropdown></CartDropdown>
+    </nav>
+  );
+}

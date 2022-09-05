1f72708206488278d3b9ad8bcd9eca8da35edce5
Veanty <plmulticraftpl@gmail.com>
2022-09-05 18:05:56 +0200

    Feat: now cart can track what items user wants to buy, calculates total cost of the selected items, shows number of selected items near the cart icon in navbar

---
+++ project-shopping-cart/src/components/CartDropdown.js
@@ -0,0 +1,50 @@
+import Dropdown from "react-bootstrap/dropdown";
+import { Link } from "react-router-dom";
+
+//Dropdown component
+export function CartDropdown({ cart }) {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="d-flex align-items-center arena text-white">
+        <span className="material-symbols-outlined cart">shopping_cart</span>
+        <span className="border-info rounded-circle">{cart.length}</span>
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu className="">
+        {cart.map((item) => {
+          return (
+            <>
+              <Link to={`/${item.itemId}`}>
+                <li>
+                  <h4>
+                    <img
+                      src={item.item.images.icon}
+                      alt={item.item.name}
+                      className="mw-100 mh-100"
+                    ></img>
+                    {item.item.name}
+                  </h4>
+                </li>
+              </Link>
+            </>
+          );
+        })}
+
+        <Dropdown.Divider></Dropdown.Divider>
+        <Dropdown.ItemText className="text-decoration-none">
+          <h3>
+            {cart.reduce((total, item) => {
+              return total + item.store.cost;
+            }, 0)}{" "}
+            <img src={require("../images/coins.png")} width={"30%"}></img>
+          </h3>
+        </Dropdown.ItemText>
+        <Link to="/cart">
+          <Dropdown.ItemText className="text-decoration-none">
+            <h5>Go to checkout</h5>
+          </Dropdown.ItemText>
+        </Link>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}

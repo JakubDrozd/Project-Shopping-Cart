ab29b17babf5b5c10e94b893cbe3f014cbed9739
Veanty <plmulticraftpl@gmail.com>
2022-09-07 18:28:57 +0200

    Refactor: remake of the navbar

--- project-shopping-cart/src/components/Nav.js
+++ project-shopping-cart/src/components/Nav.js
@@ -4,12 +4,24 @@ import { CartDropdown } from "./CartDropdown";
 
 export function Nav({ cart }) {
   return (
-    <nav className="nav">
-      <Link to="/">
-        <li>Home</li>
+    <nav className="store-nav d-flex justify-content-around align-items-center p-3 sticky-top">
+      <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+        <h1 className="display-4">
+          <li style={{ listStyle: "none" }} className="navbar-item">
+            <div className="button-wrapper">
+              <div className="button-inner">Home</div>
+            </div>
+          </li>
+        </h1>
       </Link>
-      <Link to="/shop">
-        <li>Shop</li>
+      <Link to="/shop" style={{ textDecoration: "none", color: "white" }}>
+        <h1 className="display-4">
+          <li style={{ listStyle: "none" }} className="navbar-item">
+            <div className="button-wrapper">
+              <div className="button-inner">Store</div>
+            </div>
+          </li>
+        </h1>
       </Link>
       <CartDropdown cart={cart}></CartDropdown>
     </nav>

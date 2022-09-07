130b3ab67ee2de0f2e55bdcf2e64e16874d197f1
Veanty <plmulticraftpl@gmail.com>
2022-09-07 18:29:38 +0200

    Refactor: remake of the shop page

--- project-shopping-cart/src/components/Shop.js
+++ project-shopping-cart/src/components/Shop.js
@@ -28,86 +28,93 @@ export function Shop({ handleClick }) {
     );
   }
   return (
-    <>
-      <div>
-        {/* Header*/}
-        <header className="py-5 arena">
-          <div className="container px-4 px-lg-5 my-5 ">
-            <div className="text-center text-white ">
-              <h1 className="display-4 fw-bolder">Today's store</h1>
-              <p className="lead fw-normal text-white-100 mb-0">
-                Updates at 1 AM CET
-              </p>
-            </div>
+    <div>
+      {/* Header*/}
+      <header className="py-5 arena">
+        <div className="container px-4 px-lg-5 my-5">
+          <div className="text-center text-white">
+            <h1 className="display-4 fw-bolder">Current Fortnite BR Store</h1>
+            <p className="lead fw-normal text-white-50 mb-0">
+              <i>Resets everyday at 1 AM CET</i>
+            </p>
           </div>
-        </header>
-        {/* Section*/}
-        <section className="d-flex arena">
-          <div className="container px-4 px-lg-5 mt-5">
-            <div className="d-flex justify-center flex-wrap gap-3">
-              {items
-                .sort((a, b) => b.store.cost - a.store.cost)
-                .map((item) => {
-                  if (item.store.cost === 0) {
-                    // eslint-disable-next-line array-callback-return
-                    return;
-                  }
+        </div>
+      </header>
+      {/* Section*/}
+      <section className="py-5 arena">
+        <div className="container px-4 px-lg-5 mt-5">
+          <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            {items
+              .sort((a, b) => b.store.cost - a.store.cost)
+              .map((item) => {
+                if (item.store.cost === 0) {
+                  return null;
+                } else {
                   return (
                     <div className="col mb-5" key={item.itemId}>
-                      <div className="vw-20 h-auto bg-light store-item">
+                      <div className="card h-100">
                         {/* Product image*/}
                         <Link to={`/${item.itemId}`}>
-                          <div className="bg-image">
-                            <img
-                              className="card-img-top bg-info"
-                              src={item.item.images.background}
-                              alt="#"
-                            />
-                          </div>
-                        </Link>
-                        {/* Product price*/}
-                        <Link to={`/${item.itemId}`}>
-                          <h5 className="d-flex justify-content-center align-items-center text-white pt-1 pb-1 price">
-                            {item.store.cost.toLocaleString("en-US")}{" "}
-                            <img
-                              src={require("../images/coins.png")}
-                              width={"10%"}
-                              alt={item.item.name}
-                            ></img>
-                          </h5>
+                          <img
+                            className="card-img-top"
+                            src={item.item.images.background}
+                            alt={item.item.name}
+                          />
                         </Link>
                         {/* Product details*/}
-                        <div className="card-body p-4">
+                        <div className="card-body p-0">
                           <div className="text-center">
+                            {/* Product price*/}
+                            <span className="d-flex justify-content-center align-items-center text-white pt-1 price">
+                              {item.store.cost.toLocaleString("en-US")}{" "}
+                              <img
+                                src={require("../images/coins.png")}
+                                alt="coins"
+                                width={"10%"}
+                              ></img>{" "}
+                            </span>
                             {/* Product name*/}
-                            <h5 className="fw-bolder">{item.item.name}</h5>
+                            <h5 className="fw-bolder display-5">
+                              {item.item.name}
+                            </h5>
                           </div>
                         </div>
                         {/* Product actions*/}
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                          <div className="text-center">
-                            <div
-                              className="btn btn-outline-dark mt-auto btn-lg"
-                              onClick={() => {
-                                handleClick(item);
-                              }}
-                            >
-                              Add to cart
-                            </div>
-                          </div>
+                        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                          <Link
+                            to="/shop"
+                            style={{ textDecoration: "none", color: "white" }}
+                          >
+                            <h1 className="display-4">
+                              <div className="button-wrapper">
+                                <div
+                                  className="button-inner"
+                                  onClick={() => {
+                                    handleClick(item);
+                                  }}
+                                >
+                                  Add to cart
+                                </div>
+                              </div>
+                            </h1>
+                          </Link>
                         </div>
                       </div>
                     </div>
                   );
-                })}
-            </div>
+                }
+              })}
           </div>
-        </section>
-        {/* Footer*/}
-        <footer className="store-nav d-flex justify-content-around align-items-center p-3">
-          <p className="m-0 text-center text-white">Copyright © Veanty 2022</p>
-        </footer>
-      </div>
-    </>
+        </div>
+      </section>
+      {/* Footer*/}
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright © Your Website 2022
+          </p>
+        </div>
+      </footer>
+    </div>
   );
 }

89fbb5a7d1ee28ab2e275e3c3d8abef21848858b
Veanty <plmulticraftpl@gmail.com>
2022-09-09 18:05:11 +0200

    Refactor: complete rehaul of dropdown menu, no longer uses bootstrap component

--- project-shopping-cart/src/components/CartDropdown.js
+++ project-shopping-cart/src/components/CartDropdown.js
@@ -1,63 +1,36 @@
 import Dropdown from "react-bootstrap/dropdown";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 //Dropdown component
 export function CartDropdown({ cart }) {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <Dropdown>
-      <Dropdown.Toggle>
-        <h1 className="display-4">
-          <li style={{ listStyle: "none" }}>
-            <div className="button-wrapper">
-              <div className="button-inner material-symbols-outlined cart d-flex align-items-center">
-                shopping_cart {cart.length}
-              </div>
+    <>
+      <h1 className="display-3">
+        <div
+          className="button-wrapper"
+          onClick={(e) => {
+            e.preventDefault();
+            setShowMenu(!showMenu);
+          }}
+        >
+          <div className="button-inner">
+            <div className="button-inner material-symbols-outlined cart ">
+              shopping_cart
             </div>
-          </li>
-        </h1>
-      </Dropdown.Toggle>
+          </div>
+        </div>
+      </h1>
 
-      <Dropdown.Menu>
-        {cart.map((item) => {
-          return (
-            <div key={item.itemId} className="selected-item">
-              <Link
-                to={`/${item.itemId}`}
-                style={{ textDecoration: "none", color: "black" }}
-              >
-                <img
-                  src={item.item.images.icon}
-                  alt={item.item.name}
-                  className="mw-100 mh-100"
-                ></img>
-                <div>
-                  <h5 className="display-5">{item.item.name}</h5>
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-
-        <Dropdown.Divider></Dropdown.Divider>
-        <Dropdown.ItemText className="text-decoration-none">
-          <h3>
-            {cart.reduce((total, item) => {
-              return total + item.store.cost;
-            }, 0)}{" "}
-            <img
-              src={require("../images/coins.png")}
-              width={"30%"}
-              height={"30%"}
-              alt="coins"
-            ></img>
-          </h3>
-        </Dropdown.ItemText>
-        <Link to="/cart" style={{ textDecoration: "none", color: "black" }}>
-          <Dropdown.ItemText>
-            <h5 className="navbar-item">Go to checkout</h5>
-          </Dropdown.ItemText>
-        </Link>
-      </Dropdown.Menu>
-    </Dropdown>
+      {showMenu ? (
+        <div className="menu">
+          <button>Menu Item 1</button>
+          <button>Menu Item 2</button>
+          <button>Menu Item 3</button>
+        </div>
+      ) : null}
+    </>
   );
 }

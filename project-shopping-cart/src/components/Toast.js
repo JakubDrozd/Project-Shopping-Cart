9af0d0383fe70a722d6fd7df72cef77a88afd1c4
Veanty <plmulticraftpl@gmail.com>
2022-09-13 22:01:54 +0200

    Final version of webiste's popup toast component

--- project-shopping-cart/src/components/Toast.js
+++ project-shopping-cart/src/components/Toast.js
@@ -3,7 +3,7 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Toast from "react-bootstrap/Toast";
-
+//
 export function AutohideExample() {
   const [show, setShow] = useState(false);
 

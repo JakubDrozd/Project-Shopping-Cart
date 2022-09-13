6741b4750ca3534f071ce114fe412f1f317ebec8
Veanty <plmulticraftpl@gmail.com>
2022-09-13 22:03:42 +0200

    Delete all unused imports from the final version of the project

--- project-shopping-cart/src/components/Shop.js
+++ project-shopping-cart/src/components/Shop.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { AutohideExample } from "./Toast";
 import { Loader } from "./Loader";
 //
 export function Shop({ handleClick }) {
